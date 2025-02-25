ec6121c900d5bf42408953ddcb0f602a54564928
Aaron Tom Viji <aarontoms29@gmail.com>
2025-02-26 01:06:21 +0530

    Update url.tsx

--- src/components/url.tsx
+++ src/components/url.tsx
@@ -1,6 +1,6 @@
 
 // const url = 'https://illegal-micheline-vteam-07180b15.koyeb.app/'
 // const url = 'http://127.0.0.1:5000'
-const url = 'https://29d98926678a6e2ce9138e2ed34e9eab.serveo.net/'
+const url = 'https://29d98926678a6e2ce9138e2ed34e9eab.serveo.net'
 
 export default url;
