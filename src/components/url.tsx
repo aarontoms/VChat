88b49387b763db88b4b209b4274a4f74208c9f92
Aaron Tom Viji <aarontoms29@gmail.com>
2025-03-11 12:36:17 +0530

    url change

--- src/components/url.tsx
+++ src/components/url.tsx
@@ -1,6 +1,6 @@
 
 // const url = 'https://illegal-micheline-vteam-07180b15.koyeb.app/'
 // const url = 'http://127.0.0.1:5000'
-const url = 'https://29d98926678a6e2ce9138e2ed34e9eab.serveo.net'
+const url = 'https://qb45f440-5000.inc1.devtunnels.ms/'
 
 export default url;
